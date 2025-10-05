763cc073ba3068e09af8553bb6f3766d5d2d056a
Nexonm <nikita96max@gmail.com>
2025-10-05 18:26:34 +0300

    delete todo

--- src/LexerExample.java
+++ src/LexerExample.java
@@ -4,8 +4,6 @@ import lexer.TokenPrinter;
 
 import java.util.List;
 
-// todo: create string tokenization
-// todo: Integer(42.) fix it!
 /**
  * Enhanced example usage demonstrating span-based token positioning.
  */
@@ -19,7 +17,7 @@ public class LexerExample {
         System.out.println("=== Simple test for lexer ===\n");
         String code = """
                 class Test is !!! !928
-                    var x := Integer(42.1.1.234.adb.w22) // Some comment
+                    var x := Integer(42.2) // Some comment
                     method increment() : Integer is
                         return x.Plus(1)
                     end
